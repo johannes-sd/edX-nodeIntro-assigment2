434cc28b1d620471ccc6c3f8481fdd39425cb447
Johan Martin Johannessen <johannes@sd.no>
2017-12-07 17:06:23 +0100

    Changed object demanded changes in methods and object-calls

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,7 @@
 module.exports = {
     addPost (req, res, store) {
-        let newPost = req.body;
-        let id = store.length;
+        let newPost = req.body.posts;
+        let id = store.posts.length;
         store.posts.push(newPost);
         console.log(store);
         res.status(201).send({id});
@@ -11,8 +11,8 @@ module.exports = {
         let postIndex = req.params.postId;
         let commentIndex = store.posts[postIndex].comments.length;
         //console.log(store.posts[postIndex].comments);
+        console.log(req.body.posts);
         store.posts[postIndex].comments.push(req.body);
-        console.log(store.posts);
         res.status(201).send(store.posts);
     }
 }
