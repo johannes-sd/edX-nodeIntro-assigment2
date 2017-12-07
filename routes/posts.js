efbb8e351ca0a492c121ce70faa8596515676841
Johan Martin Johannessen <jojo@sd.no>
2017-12-07 15:29:26 +0100

    Added addComment and had to change the store object

--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,18 @@
 module.exports = {
     addPost (req, res, store) {
-        console.log("Prosessing post request");
         let newPost = req.body;
-        console.log(newPost);
         let id = store.length;
-        console.log(id);
         store.posts.push(newPost);
         console.log(store);
         res.status(201).send({id});
-    } 
+    },
 
+    addComment (req, res, store) {
+        let postIndex = req.params.postId;
+        let commentIndex = store.posts[postIndex].comments.length;
+        //console.log(store.posts[postIndex].comments);
+        store.posts[postIndex].comments.push(req.body);
+        console.log(store.posts);
+        res.status(201).send(store.posts);
+    }
 }
--- server.js
+++ server.js
@@ -8,11 +8,8 @@ let store = {
       {name: 'Top 10 ES6 Features every Web Developer must know',
       url: 'https://webapplog.com/es6',
       text: 'This essay will give you a quick introduction to ES6. If you don’t know what is ES6, it’s a new JavaScript implementation.',
-      comments: [{
-        text: 'Cruel…..var { house, mouse} = No type optimization at all',
-        text: 'I think you’re undervaluing the benefit of ‘let’ and ‘const’.',
-        text: '(p1,p2)=>{ … } ,i understand this ,thank you !'      
-    }]
+      comments: 
+        ['Cruel…..var { house, mouse} = No type optimization at all', 'I think you’re undervaluing the benefit of ‘let’ and ‘const’.', '(p1,p2)=>{ … } ,i understand this ,thank you !']
       }
     ]
   };
@@ -29,7 +26,7 @@ app.get('/posts', (req, res) => {
     res.status(200).send(posts);
 });
 
-app.post('/posts', (req, res) => {routes.post.addPost(req, res, store)});
+app.post('/posts', (req, res) => {routes.post.addPost(req, res, store)}); //Passing the store-object.
 
 app.put('/posts/:postId', (req, res) => {
     store.accounts[req.params.id] = req.body;
@@ -45,12 +42,7 @@ app.get('/posts/:postId/comments', (req, res) => {
     res.status(200).send(store.accounts);
 });
 
-app.post('/posts/:postId/comments', (req, res) => {
-    let newAccount = req.body;
-    let id = store.accounts.length;
-    store.accounts.push(newAccount);
-    res.status(201).send({id});
-});
+app.post('/posts/:postId/comments', (req, res) => { routes.post.addComment(req, res, store); });
 
 
 app.listen(3000);
